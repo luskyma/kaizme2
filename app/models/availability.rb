7398915d409e24a7f9cba574923e30ec37c9527e
MAL <marvinlusky@gmail.com>
2015-05-23 11:51:48 -0400

    update to availability and appointment model

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,5 +1,5 @@
 class Availability < ActiveRecord::Base
   belongs_to :provider
 
-  scope :for_provider, ->(user_id) { Availability.where provider_id: user_id }
+  scope :by_provider, ->(user_id) { Availability.where provider_id: user_id }
 end
