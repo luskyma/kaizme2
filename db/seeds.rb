216f6f4ce2875d3048debaa6d0876d3ef57185bd
MAL <marvinlusky@gmail.com>
2015-05-27 20:08:28 -0400

    added link to Tod create session and added seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create({first_name: "Dr. Sigmund", last_name: "Freud", email: "provider@example.com", password: "1234567890", is_provider: true})
+User.create({first_name: "Dolores", last_name: "De Cabeza", email: "patient@example.com", password: "1234567890"})
