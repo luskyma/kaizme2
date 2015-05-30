15230e251bbd40753e154fa119d3b15deff824ca
Gabe <gabriel.g.noboa@gmail.com>
2015-05-30 19:13:17 -0400

    added paperclip

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,19 +1,21 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+
 def after_update_path_for(resource)
     current_user.is_provider ? dashboard_providers_path : dashboard_patients_path
-  end
+end
+
 
 private
 
 def sign_up_params
   params.require(:user).permit(:email, :first_name, :last_name, :phone_number, :password,
-              :password_confirmation, :current_password, :is_provider, :bio, :dob, :gender)
+              :password_confirmation, :current_password, :is_provider, :bio, :dob, :gender, :avatar)
 end
 
 def account_update_params
   params.require(:user).permit(:email, :first_name, :last_name, :phone_number, :password,
-              :password_confirmation, :current_password, :is_provider, :bio, :dob, :gender)
+              :password_confirmation, :current_password, :is_provider, :bio, :dob, :gender, :avatar)
 end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,13 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+
 
 module Kaizme
   class Application < Rails::Application
